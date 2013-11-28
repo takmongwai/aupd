1497bbaff53e78d7423c32bb1a3baad1b008ffe9
dewang wei <me@weidewang.com>
2013-11-28 14:48:20 +0800

    sort params

--- src/aupd/http_proxy_handler.go
+++ src/aupd/http_proxy_handler.go
@@ -1,64 +1,72 @@
 package main
 
 import (
-  _ "cache"
-  "io"
-  _ "io/ioutil"
-  "log"
-  "net"
-  "net/http"
-  "time"
+	"cache"
+	"io"
+	_ "io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+const (
+	CONNECTION_TIME_OUT     = 15
+	RESPONSE_TIME_OUT       = 60
+	MAX_IDLE_CONNS_PRE_HOST = 200
+	DISABLE_COMPRESSION     = false
+	DISABLE_KEEP_ALIVES     = false
 )
 
 var transport = http.Transport{
-  Dial: func(nework, addr string) (net.Conn, error) {
-    return net.DialTimeout(nework, addr, time.Duration(20)*time.Second)
-  },
-  ResponseHeaderTimeout: time.Duration(60) * time.Second,
-  DisableCompression:    false,
-  DisableKeepAlives:     false,
-  MaxIdleConnsPerHost:   200,
+	Dial: func(nework, addr string) (net.Conn, error) {
+		return net.DialTimeout(nework, addr, time.Duration(CONNECTION_TIME_OUT)*time.Second)
+	},
+	ResponseHeaderTimeout: time.Duration(RESPONSE_TIME_OUT) * time.Second,
+	DisableCompression:    DISABLE_COMPRESSION,
+	DisableKeepAlives:     DISABLE_KEEP_ALIVES,
+	MaxIdleConnsPerHost:   MAX_IDLE_CONNS_PRE_HOST,
 }
 
 var client = &http.Client{
-    Transport: &transport,
-  }
+	Transport: &transport,
+}
 
 func headerCopy(s http.Header, d *http.Header) {
-  for hk, _ := range s {
-    d.Set(hk, s.Get(hk))
-  }
+	for hk, _ := range s {
+		d.Set(hk, s.Get(hk))
+	}
 }
 
 func showError(w http.ResponseWriter, msg []byte) {
-  w.WriteHeader(500)
-  w.Write(msg)
+	w.WriteHeader(500)
+	w.Write(msg)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  
-  log.Println("----------------------------------")
-  log.Println("RequestURI", r.RequestURI)
-  log.Println("RemoteAddr", r.RemoteAddr)
-  log.Println("----------------------------------")
 
-  req, err := http.NewRequest(r.Method, r.URL.String(), r.Body)
-  headerCopy(r.Header, &req.Header)
-  defer func() { req.Close = true }()
-  if err != nil {
-    showError(w, []byte(err.Error()))
-    return
-  }
-  resp, err := client.Do(req)
-  defer resp.Body.Close()
-  if err != nil {
-    showError(w, []byte(err.Error()))
-    return
-  }
-  for hk, _ := range resp.Header {
-    w.Header().Set(hk, resp.Header.Get(hk))
-  }
-  w.WriteHeader(resp.StatusCode)
-  io.Copy(w, resp.Body)
+	log.Println("----------------------------------")
+	log.Println("RequestURI", r.RequestURI)
+	log.Println("RemoteAddr", r.RemoteAddr)
+	log.Println("----------------------------------")
+	cache.GenKey(r)
 
+	req, err := http.NewRequest(r.Method, r.URL.String(), r.Body)
+	headerCopy(r.Header, &req.Header)
+	defer func() { req.Close = true }()
+	if err != nil {
+		showError(w, []byte(err.Error()))
+		return
+	}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		showError(w, []byte(err.Error()))
+		return
+	}
+	for hk, _ := range resp.Header {
+		w.Header().Set(hk, resp.Header.Get(hk))
+	}
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
 }
--- src/cache/key.go
+++ src/cache/key.go
@@ -1,20 +1,28 @@
 package cache
 
 import (
-  "crypto/md5"
-  "fmt"
-  "io"
-  "log"
-  "net/http"
-  "strings"
+	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"sort"
+	_ "strings"
 )
 
-func parseQuerys(r *http.Request) (rawQuery []string) {
-  r.ParseForm()
-  for k, _ := range r.Form {
-    rawQuery = append(rawQuery, fmt.Sprintf("%s=%s", k, r.Form.Get(k)))
-  }
-  return
+func parseQuerys(r *http.Request) (rawQuery sort.StringSlice) {
+	r.ParseForm()
+	for k, _ := range r.Form {
+		rawQuery = append(rawQuery, fmt.Sprintf("%s=%s", k, r.Form.Get(k)))
+	}
+
+	var st sort.IntSlice
+	st = append(st, sort.IntSlice{90, 9, 18, 0, 100}...)
+	log.Println(st)
+
+	//log.Println(rawQuery.sort())
+
+	return
 }
 
 /*
@@ -22,12 +30,12 @@ func parseQuerys(r *http.Request) (rawQuery []string) {
 (URL + 排序过的参数(含post)).md5
 */
 func GenKey(r *http.Request) string {
-  log.Println(parseQuerys(r))
-  return str_md5("hello")
+	log.Println(parseQuerys(r))
+	return str_md5("hello")
 }
 
 func str_md5(s string) string {
-  h := md5.New()
-  io.WriteString(h, s)
-  return fmt.Sprintf("%x", h.Sum(nil))
+	h := md5.New()
+	io.WriteString(h, s)
+	return fmt.Sprintf("%x", h.Sum(nil))
 }
