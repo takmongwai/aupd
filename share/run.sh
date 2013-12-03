8e95f443a548999464da5f8b1d10bf45301e97e2
dewang wei <dewang.wei@gmail.com>
2013-12-03 11:00:18 +0800

    修改启动脚本

--- share/run.sh
+++ share/run.sh
@@ -3,6 +3,6 @@ ulimit -n 655350
 
 export GOMAXPROCS=`cat /proc/cpuinfo | grep -v grep | grep process | wc -l`
 export GOGCTRACE=1
-export GODEBUG=1
+export GODEBUG=gctrace=1
 
-GODEBUG=1 gctrace=1 GOGCTRACE=1 GOMAXPROCS=8 /srv/aupd/bin/aupd 
+GODEBUG=gctrace=1 GOGCTRACE=1 GOMAXPROCS=8 /srv/aupd/bin/aupd 
