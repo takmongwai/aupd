3796902dfa2022e3110657ca70027b3be74dc70d
root <root@525400e07678.(none)>
2013-12-03 14:57:57 +0800

    添加忽略文件 修改运行参数

--- share/run.sh
+++ share/run.sh
@@ -2,7 +2,6 @@
 ulimit -n 655350
 
 export GOMAXPROCS=`cat /proc/cpuinfo | grep -v grep | grep process | wc -l`
-export GOGCTRACE=1
-export GODEBUG=gctrace=1
+export GODEBUG='gctrace=1'
 
-GODEBUG=gctrace=1 GOGCTRACE=1 GOMAXPROCS=8 /srv/aupd/bin/aupd 
+GODEBUG='gctrace=1,schedtrace=1,scheddetail'  GOMAXPROCS=8 /srv/aupd/bin/aupd 
