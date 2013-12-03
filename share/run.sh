fc99635e0faabde53cefccfbbb7cf8bb015b75c7
root <root@525400e07678.(none)>
2013-12-03 16:27:48 +0800

    修改启动脚本

--- share/run.sh
+++ share/run.sh
@@ -2,5 +2,6 @@
 ulimit -n 655350
 
 export GOMAXPROCS=`cat /proc/cpuinfo | grep -v grep | grep process | wc -l`
-export GODEBUG='gctrace=1,schedtrace=1,scheddetail'
+#export GODEBUG='gctrace=1,schedtrace=1,scheddetail=1'
+export GODEBUG='gctrace=1,scheddetail=1'
 /srv/aupd/bin/aupd 
