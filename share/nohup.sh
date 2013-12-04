df1aba7fd2003ee6b8e3bf357cdc4c0a577e813f
root <root@525400e07678.(none)>
2013-12-05 02:50:26 +0800

    modified:   share/nohup.sh modified:   share/run.sh

--- share/nohup.sh
+++ share/nohup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kill -9 `ps aux | grep -v grep | grep "/srv/aupd/bin/aupd" | awk '{print $2}'`
-
+#kill -9 `ps aux | grep -v grep | grep "/srv/aupd/bin/aupd" | awk '{print $2}'`
+[[ -s "/srv/aupd/tmp/aupd.pid" ]] &&  kill -9 `cat /srv/aupd/tmp/aupd.pid`
 #nohup /srv/aupd/share/run.sh 2>/dev/null 1>/dev/null &
 nohup /srv/aupd/share/run.sh 2>&1 >> /srv/aupd/log/stdout.log &
--- share/run.sh
+++ share/run.sh
@@ -4,4 +4,12 @@ ulimit -n 655350
 export GOMAXPROCS=`cat /proc/cpuinfo | grep -v grep | grep process | wc -l`
 #export GODEBUG='gctrace=1,schedtrace=1,scheddetail=1'
 export GODEBUG='gctrace=1,scheddetail=1'
-/srv/aupd/bin/aupd 
+#/srv/aupd/bin/aupd 
+/srv/aupd/bin/aupd  -log=/srv/aupd/log/stdout.log -pid=/srv/aupd/tmp/aupd.pid
+
+
+#Usage: /srv/aupd/bin/aupd
+#  -h="0.0.0.0": listen host
+#  -log="": log file
+#  -p=8000: listen port
+#  -pid="": pid file
