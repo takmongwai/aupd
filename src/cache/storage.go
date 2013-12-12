b407277d7b30c71b79ea47335e1540d719ce26e6
root <root@525400e07678.(none)>
2013-12-13 00:51:00 +0800

    modified:   src/cache/storage.go

--- src/cache/storage.go
+++ src/cache/storage.go
@@ -16,8 +16,8 @@ const (
   STATUS_UPDATING             = 2
   ENTITY_UPDATE_DURATION      = 60 //Second,每个缓存需要更新的时间,依据最后更新时间和当前时间计算
   ENTITY_DURATION             = 3600
-  CLIENT_LAST_ACCESS_DURATION = 3600 //Second,每个缓存的持续时间,依据最后访问时间和当前时间计算
-  MAX_CONCURRENT              = 15   //每次更新的条目数,每次返回需要更新的条目数不超过该设定
+  CLIENT_LAST_ACCESS_DURATION = 1800 //Second,每个缓存的持续时间,依据最后访问时间和当前时间计算
+  MAX_CONCURRENT              = 25   //每次更新的条目数,每次返回需要更新的条目数不超过该设定
 )
 
 type ResponseStorage struct { //响应体
